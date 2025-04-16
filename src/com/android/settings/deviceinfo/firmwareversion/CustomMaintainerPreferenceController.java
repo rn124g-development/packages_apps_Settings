2f18d9d2e8c79e82abdfb93544c941afc7257b67
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-04-16 22:43:16 +0530

    Settings: Allow maintainers to use overlay for maintainer string
    fallback order:
    First preference: ro.custom.maintainer system property.
    If empty: R.string.aospb_maintainer from strings.xml.
    If still empty: R.string.device_info_default from strings.xml.
    
    Change-Id: I32a7e7e302ce6c86ca5046e0ed70453d7dcaee7e
    Signed-off-by: sajidshahriar72543 <sazidshahriar39@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/CustomMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/CustomMaintainerPreferenceController.java
@@ -17,6 +17,7 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.SystemProperties;
 import android.text.TextUtils;
 
@@ -40,7 +41,11 @@ public class CustomMaintainerPreferenceController extends BasePreferenceControll
 
     public CharSequence getSummary() {
         String maintainer = SystemProperties.get(MAINTAINER_PROPERTY,
-                this.mContext.getString(R.string.device_info_default));
+                mContext.getResources().getString(R.string.custom_maintainer));
+
+        if (TextUtils.isEmpty(maintainer)) {
+            maintainer = mContext.getString(R.string.device_info_default);
+        }
         return maintainer;
     }
 }
